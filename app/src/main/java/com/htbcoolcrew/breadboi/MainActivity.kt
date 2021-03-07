c69365dfa4039b1c78e4ad578bf603314420ffd8
JK <jonathankane1204@yahoo.co.uk>
2021-03-07 03:10:32 +0000

    main imports to access the cloud vision there just need to use em

--- app/src/main/java/com/htbcoolcrew/breadboi/MainActivity.kt
+++ app/src/main/java/com/htbcoolcrew/breadboi/MainActivity.kt
@@ -25,18 +25,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hasCameraPermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.CAMERA
-        ) == PackageManager.PERMISSION_GRANTED
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+//                (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
     }
 
     private fun requestPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            CAMERA_PERMISSION,
-            CAMERA_REQUEST_CODE
-        )
+        ActivityCompat.requestPermissions(this, CAMERA_PERMISSION, CAMERA_REQUEST_CODE)
+//        ActivityCompat.requestPermissions(this, NET_PERMS, NET_REQUEST_CODE)
+//        ActivityCompat.requestPermissions(this, WRITE_PERM, STORAGE_PERMISSION_CODE)
+
     }
 
     private fun enableCamera() {
@@ -45,8 +42,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private val CAMERA_PERMISSION =
-            arrayOf(Manifest.permission.CAMERA)
+        private val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+        private val NET_PERMS = arrayOf(Manifest.permission.INTERNET)
+        private val WRITE_PERM = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        private val READ_PERM = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
         private const val CAMERA_REQUEST_CODE = 10
+        private const val NET_REQUEST_CODE = 11
+        private const val STORAGE_PERMISSION_CODE = 101
     }
 }
